4a8f8cac6863570b310fc6bbb4ab803a974da591
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-04-25 09:53:46 -0400

    Added Map View Page

--- Roots/App.js
+++ Roots/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+      return (
+        <MapView style = {{flex: 1}}
+        provider = "google"
+        region = {{
+            latitude: 40.76727216,
+            longitude: -73.99392888,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        />
+      );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
