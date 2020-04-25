13cabea336c78d56c095e3198757486b41475b84
caixunshiren <caixunshiren@gmail.com>
2020-04-25 15:58:22 -0400

    Update FinanceScreen.js

--- Roots/screens/FinanceScreen.js
+++ Roots/screens/FinanceScreen.js
@@ -1,16 +1,60 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { Image, Platform, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import { Ionicons } from '@expo/vector-icons';
 
 import { MonoText } from '../components/StyledText';
 
+var balance = 77.00;
+balance = balance.toFixed(2)
 
-export default function FinanceScreen() {
+export default function FinanceScreen (){
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Hello, world!</Text>
-  </View>
+  <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+
+    <View style={styles.balanceViewText}>
+    <Text style = {styles.balanceText} >Your Balance</Text>
+    </View>
+    
+    <View style={styles.balanceViewNumber}>
+      <Text style = {styles.balanceNumber}>    {balance}</Text>
+      <Text style = {styles.usd}>usd</Text>
+    </View>
+
+    <OptionButton
+        icon="ios-wallet"
+        label="Top Up"
+        onPress={() => alert('You tapped the button!')}
+    />
+
+    <OptionButton
+        icon="md-wallet"
+        label="Withdraw"
+        onPress={() => alert('You tapped the button!')}
+        isLastOption
+    />
+
+    <View style={styles.communityDonation}>
+      
+    </View>
+
+  </ScrollView>
+  );
+}
+
+function OptionButton({ icon, label, onPress, isLastOption }) {
+  return (
+    <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
+      <View style={{ flexDirection: 'row' }}>
+        <View style={styles.optionIconContainer}>
+          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
+        </View>
+        <View style={styles.optionTextContainer}>
+          <Text style={styles.optionText}>{label}</Text>
+        </View>
+      </View>
+    </RectButton>
   );
 }
 
@@ -19,90 +63,52 @@ FinanceScreen.navigationOptions = {
 };
 
 const styles = StyleSheet.create({
+  contentContainer: {
+    paddingTop: 40,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white'
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+  balanceViewText : {
+    flex: 2, justifyContent: 'flex-start', alignItems: 'center', backgroundColor: 'white', paddingBottom:13
   },
-  contentContainer: {
-    paddingTop: 30,
+  balanceText : {
+    fontSize: 20, fontWeight: "bold"
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+  balanceNumber : {
+    fontSize: 60, fontWeight: '600'
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+  usd : {
+    fontSize: 30, fontWeight: "200"
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
+  balanceViewNumber : {
+    flex: 2, justifyContent: 'center', alignItems: 'flex-end', backgroundColor: 'white', flexDirection: 'row', paddingBottom: 8
   },
-  homeScreenFilename: {
-    marginVertical: 7,
+
+  communityDonation : {
+    flex: 10,
   },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
+
+
+
+  optionIconContainer: {
+    marginRight: 12,
   },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
+  option: {
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 15,
     paddingVertical: 15,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderBottomWidth: 0,
+    borderColor: '#ededed',
   },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  lastOption: {
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  optionText: {
+    fontSize: 15,
+    alignSelf: 'flex-start',
+    marginTop: 1,
   },
 });
