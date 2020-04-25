90dd7bae40661fe1ec1f257212745ae82e827bb3
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-04-25 16:02:22 -0400

    Updated Firebase Dependencies

--- Roots/screens/LinksScreen.js
+++ Roots/screens/LinksScreen.js
@@ -1,48 +1,50 @@
 import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 
-export default function LinksScreen() {
-    return (
+import * as firebase from 'firebase';
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmIzUERgQIiesJMiulSvn9ZNTq0GP6aV0",
+  authDomain: "roots-275313.firebaseapp.com",
+  databaseURL: "https://roots-275313.firebaseio.com",
+  projectId: "roots-275313",
+  storageBucket: "roots-275313.appspot.com",
+  messagingSenderId: "332657243073",
+  appId: "1:332657243073:web:9a539b8c7a77607d833540",
+  measurementId: "G-9QPPJXPG9D"
+};
+
+// Initialize the firebase app
+firebase.initializeApp(firebaseConfig);
+
+// Check to see if the current user is logged in -- show different pages if so
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    first_time = false;
+  }
+});
+
+export default class LinksScreen extends Component {
+
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+
+    return(
       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <OptionButton
-          icon="md-school"
-          label="Read the Expo documentation"
-          onPress={() => WebBrowser.openBrowserAsync('https://docs.expo.io')}
-        />
-
-        <OptionButton
-          icon="md-compass"
-          label="Read the React Navigation documentation"
-          onPress={() => WebBrowser.openBrowserAsync('https://reactnavigation.org')}
-        />
-
-        <OptionButton
-          icon="ios-chatboxes"
-          label="Ask a question on the forums"
-          onPress={() => WebBrowser.openBrowserAsync('https://forums.expo.io')}
-          isLastOption
-        />
+      
       </ScrollView>
     );
 
-}
+  }
 
-function OptionButton({ icon, label, onPress, isLastOption }) {
-  return (
-    <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={styles.optionIconContainer}>
-          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
-        </View>
-        <View style={styles.optionTextContainer}>
-          <Text style={styles.optionText}>{label}</Text>
-        </View>
-      </View>
-    </RectButton>
-  );
 }
 
 const styles = StyleSheet.create({
